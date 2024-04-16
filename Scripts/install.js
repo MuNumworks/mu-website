66946eba0589758914ee7001407c9f8c70b3c271
MuNumworks <167102037+MuNumworks@users.noreply.github.com>
2024-04-16 16:32:24 +0000

    Encore le test pour install des bins

--- Scripts/install.js
+++ Scripts/install.js
@@ -1,10 +1,9 @@
-//import Numworks from "numworks.js";
-
 var calculator = new Numworks();
 
 var status = document.getElementById("status");
 var connect = document.getElementById("connect");
 var content = document.getElementById("content");
+var install = document.getElementById("installMu");
 
 navigator.usb.addEventListener("disconnect", function (e) {
   calculator.onUnexpectedDisconnect(e, function () {
@@ -34,13 +33,73 @@ connect.onclick = function (e) {
   );
 };
 
+install.onclick = function (e) {
+  console.log("Test Install");
+  flashEpsilonOnboardingA();
+};
+
 async function connected() {
   connect.disabled = true;
   status.innerHTML = "Connected.";
 
-  var model = calculator.getModel(false);
+  var connectDiv = document.getElementById("connect");
+  connectDiv.style.display = "none";
 
   var html_content = "Info : " + calculator.getModel(false) + "<br/>";
 
   content.innerHTML = html_content;
+}
+
+async function flashEpsilonOnboardingA() {
+  // Chemin d'accès au fichier .bin
+  const cheminFichier = "../Bins/Chrys130/epsilon.onboarding.A.bin";
+
+  try {
+    const response = await fetch("../Bins/Chrys122/epsilon.onboarding.A.bin");
+    if (!response.ok) {
+      throw new Error("Failed to load binary file");
+    }
+
+    const buffer = await response.arrayBuffer();
+    //displayFileContents(buffer);
+
+    // Appelez la fonction flashExternal avec votre buffer
+    await calculator.flashExternal(buffer);
+
+    console.log(
+      "Flashing epsilon.onboarding.A.bin to external memory successful!",
+    );
+  } catch (error) {
+    console.error(
+      "Error while flashing epsilon.onboarding.A.bin to external memory:",
+      error,
+    );
+  }
+}
+
+async function getPy() {
+  let data = await calculator.backupStorage();
+
+  var scriptsContainer = document.getElementById("scriptsContainer");
+
+  for (var i = 0; i < data.records.length; i++) {
+    var record = data.records[i];
+
+    // Créer une div pour afficher le script
+    var scriptDiv = document.createElement("div");
+    scriptDiv.classList.add("script");
+
+    // Créer un titre avec le nom du script
+    var scriptTitle = document.createElement("h2");
+    scriptTitle.textContent = record.name;
+    scriptDiv.appendChild(scriptTitle);
+
+    // Créer un paragraphe pour afficher le contenu du script
+    var scriptContent = document.createElement("p");
+    scriptContent.textContent = record.code;
+    scriptDiv.appendChild(scriptContent);
+
+    // Ajouter la div du script à la page
+    scriptsContainer.appendChild(scriptDiv);
+  }
 }
