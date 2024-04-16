3b0d04dd694267571a3de600c085dd63ab074c3e
MuNumworks <167102037+MuNumworks@users.noreply.github.com>
2024-04-16 09:03:48 +0000

    Added Upsilon.js for detect calculator

---
+++ Scripts/install.js
@@ -0,0 +1,46 @@
+//import Numworks from "numworks.js";
+
+var calculator = new Numworks();
+
+var status = document.getElementById("status");
+var connect = document.getElementById("connect");
+var content = document.getElementById("content");
+
+navigator.usb.addEventListener("disconnect", function (e) {
+  calculator.onUnexpectedDisconnect(e, function () {
+    status.innerHTML = "Disconnected.";
+    content.innerHTML = "Please connect your Numworks.";
+    connect.disabled = false;
+    calculator.autoConnect(autoConnectHandler);
+  });
+});
+
+calculator.autoConnect(autoConnectHandler);
+
+function autoConnectHandler(e) {
+  calculator.stopAutoConnect();
+  connected();
+}
+
+connect.onclick = function (e) {
+  calculator.detect(
+    function () {
+      calculator.stopAutoConnect();
+      connected();
+    },
+    function (error) {
+      status.innerHTML = "Error: " + error;
+    },
+  );
+};
+
+async function connected() {
+  connect.disabled = true;
+  status.innerHTML = "Connected.";
+
+  var model = calculator.getModel(false);
+
+  var html_content = "Info : " + calculator.getModel(false) + "<br/>";
+
+  content.innerHTML = html_content;
+}
